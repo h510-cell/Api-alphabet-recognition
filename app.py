016aeaab41677df97977fb6b8f0a8fe20965656e
h510-cell <58731327+h510-cell@users.noreply.github.com>
2020-10-29 16:50:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+from classifier import get_pridiction
+
+app = Flask(__name__)
+
+@app_route("/predict_digit", methods = ["Post"])
+def predict_data():
+    # image = cv2.imdecode(np.fromstring(request.files.get("digit").read(), np.unit8), cv2.IMREAD_UNCHANGED)
+    image = request.files.get("digit")
+    prediction = get_pridiction(image)
+    return jsonify({
+        "prediction" : prediction
+}),200
+
+if __name__ == "__main__":
+    app.run(debug =True)
